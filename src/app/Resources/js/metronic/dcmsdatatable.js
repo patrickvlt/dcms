192e3586b6285726c2cee55c8a8f465c86cace1f
Patrick Veltrop <patrick@jottenheijm.com>
2020-08-12 00:39:24 +0200

    Use first key in array with images

--- src/app/Resources/js/metronic/dcmsdatatable.js
+++ src/app/Resources/js/metronic/dcmsdatatable.js
@@ -142,10 +142,11 @@ window.DCMSDatatable = function (parameters) {
 								deleteControl = '';
 							} 
 							spotlightClass = (value !== null) ? 'spotlight' : '';
-							
-							if (value instanceof Array){
+
+							if (value instanceof Array && value.length > 1){
 								value = value[0];
 							}
+
 							return `<div class="image-input mb-4 mt-4" data-id='`+row.id+`' style="max-height:`+column.dataset.maxHeight+`">
 								<div class="image-input-wrapper `+spotlightClass+`" data-src='`+value+`' style="background-image: url(`+value+`)"></div>
 								`+changeControl+`
