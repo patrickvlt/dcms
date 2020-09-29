6eedc46e48204afaaab1fcae15434d9a2f5cd28a
Patrick <patrickveltrop@live.nl>
2020-09-29 13:21:37 +0200

    Include dcms.json

--- src/app/Console/Commands/Publish.php
+++ src/app/Console/Commands/Publish.php
@@ -29,16 +29,25 @@ class Publish extends Command
     {
         $console = $this;
         $rootjs = base_path() . '/resources/js/dcms/';
+        $rootjsassets = base_path() . '/public/js/dcms/assets';
         $rootscss = base_path() . '/resources/sass/dcms/';
+        $rootcssassets = base_path() . '/public/css/dcms/assets';
         $vendorjs = base_path() . '/vendor/pveltrop/dcms/src/app/Resources/js';
+        $vendorjsassets = base_path() . '/vendor/pveltrop/dcms/src/app/Public/js/assets';
         $vendorscss = base_path() . '/vendor/pveltrop/dcms/src/app/Resources/sass';
+        $vendorcssassets = base_path() . '/vendor/pveltrop/dcms/src/app/Public/css/assets';
 
         RemoveDir($rootjs);
+        RemoveDir($rootjsassets);
         RemoveDir($rootscss);
+        RemoveDir($rootcssassets);
 
         CopyDir($vendorjs,$rootjs);
+        CopyDir($vendorjsassets,$rootjsassets);
         CopyDir($vendorscss,$rootscss);
+        CopyDir($vendorcssassets,$rootcssassets);
         copy(base_path() . '/vendor/pveltrop/dcms/src/Config/dcms.php', base_path() . '/config/dcms.php');
+        copy(base_path() . '/vendor/pveltrop/dcms/src/dcms.json', base_path() . '/dcms.json');
 
         print("\n".shell_exec('composer show pveltrop/dcms --all')."\n");
         print("\n".shell_exec('git status')."\n");
