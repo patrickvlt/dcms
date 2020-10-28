2165c0ec31c909df6ece00fa3aae50c83de24e31
Patrick Veltrop <patrickveltrop@live.nl>
2020-10-28 14:02:16 +0100

    Datatable filter uses one query to fetch, PHP filters records now

--- src/app/Classes/Datatable.php
+++ src/app/Classes/Datatable.php
@@ -13,6 +13,7 @@ class Datatable
     public function __construct($query, $searchFields=null){
         $this->query = $query;
         $this->searchFields = $searchFields;
+        $this->data = null;
     }
 
     /**
@@ -22,9 +23,20 @@ class Datatable
      * @param $value
      */
 
-    public function filter($field=[], $value=[])
+    public function filter($field=null, $value=null)
     {
-        return $this->query->where($field, '=', $value);
+        $this->data = array_filter($this->data, function($row) use ($field, $value) {
+            // Build your datatable filter here
+            switch ($field) {
+                // case 'field_price_example':
+                //     return ($row[$field] <= $value) ? $row : null;
+                //     break;
+        
+                default:
+                    return ($row[$field] == $value) ? $row : null;
+                    break;
+            }
+        });;
     }
 
     /**
@@ -39,7 +51,19 @@ class Datatable
         // Get parameters from request
         $params = request()->all();
 
-        // Execute query
+        $this->data = $this->query->get();
+
+        // Sort all collected data first with Laravel's sortBy method
+        if (isset($params['sort'])) {
+            // orderBy(field,asc)
+            $sortBy = ($params['sort']['sort'] == 'asc') ? 'sortBy' : 'sortByDesc';
+            $data = $this->data->{$sortBy}($params['sort']['field']);
+        }
+
+        // Convert collection to array
+        $this->data = array_values($this->data->toArray());
+
+        // Filter through array
         if (isset($params['query'])) {
             foreach ($params['query'] as $key => $value) {
                 if ($key !== 'generalSearch'){
@@ -47,16 +71,6 @@ class Datatable
                 }
             }
         }
-
-        $data = $this->query->get();
-
-        if (isset($params['sort'])) {
-            // orderBy(field,asc)
-            $sortBy = ($params['sort']['sort'] == 'asc') ? 'sortBy' : 'sortByDesc';
-            $data = $data->{$sortBy}($params['sort']['field']);
-        }
-
-        $data = array_values($data->toArray());
 
         // Perform general search on remaining results
         if (isset($params['query']['generalSearch'])){
@@ -66,7 +80,7 @@ class Datatable
             if (isset($this->searchFields)){
                 $searchColumns = $this->searchFields;
             } else {
-                foreach ($data[0] as $key => $val){
+                foreach ($this->data[0] as $key => $val){
                     $searchColumns[] = $key;
                 }
             }
@@ -76,7 +90,7 @@ class Datatable
             // First, flatten the array so no nested values remain
             // Then preg match the array keys with the search field
             // Then preg match the search value with the matched row in the array
-            foreach (Flatten($data) as $flatKey => $flatValue){
+            foreach (Flatten($this->data) as $flatKey => $flatValue){
                 $dataRow = explode('.',$flatKey)[0];
                 foreach($searchColumns as $x => $searchField){
                     // Make new search field by exploding . and grabbing the last element
@@ -88,7 +102,7 @@ class Datatable
                     if (preg_match('/'.strtolower($searchField).'/m', strtolower($flatKey)) > 0){
                         // Check if search value is found, then push to new array
                         if (!in_array($dataRow,$addedRows) && $flatValue !== null && $flatValue !== '' && preg_match('/'.strtolower($searchValue).'/m', strtolower($flatValue)) > 0){
-                            $newData[] = $data[$dataRow];
+                            $newData[] = $this->data[$dataRow];
                             $addedRows[] = $dataRow;
                         }
                     }
@@ -97,19 +111,19 @@ class Datatable
 
             // Clear data if general search cant find anything
             // Or else the results wont be affected
-            $data = (count($data) >! 0) ? [] : $newData;
+            $this->data = (count($this->data) >! 0) ? [] : $newData;
         }
 
         // Retrieve pagination parameters, to paginate the results and return a meta response
-        $total = count($data);
+        $total = count($this->data);
         $perPage = isset($params['pagination']['perpage']) && $params['pagination']['perpage'] !== 'NaN' ? $params['pagination']['perpage'] : null;
         $page = isset($params['pagination']['page']) ? $params['pagination']['page']-1 : null;
 
         // Make response object with meta
         $response = (object) '';
         // Paginate the results if page and perpage parameters are present
-        if (isset($page,$perPage) && count($data) > 0){
-            $paginatedData = array_chunk($data, $perPage, true);
+        if (isset($page,$perPage) && count($this->data) > 0){
+            $paginatedData = array_chunk($this->data, $perPage, true);
             $pages = count($paginatedData);
             // If page/key exists in paginated data array, return this data
             if (isset($paginatedData[$page])){
@@ -129,7 +143,7 @@ class Datatable
             ];
         } else {
             // Return all data if no pagination parameters are present
-            $response->data = $data;
+            $response->data = $this->data;
         }
 
         return response()->json($response);
