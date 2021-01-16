796f494886dc898812fc4c122d0fa5b37ee5f54c
Patrick Veltrop <patrickveltrop@live.nl>
2021-01-16 17:16:25 +0100

    Updated dcms config example

--- src/Config/dcms.php
+++ src/Config/dcms.php
@@ -12,4 +12,24 @@ return [
     */
 
     'modelFolders' => ['app/Models'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Storage
+    |--------------------------------------------------------------------------
+    |
+    | Configure storage service, globally or per model.
+    | When changing storage service for a specific model, define an additional
+    | array entry: ['storage']['service'][(route prefix/name of model)]
+    |
+    | available storage services: laravel & dropbox
+    |
+    */
+
+    'storage' => [
+        'service' => [
+            'global' => 'laravel'
+            // 'post' => 'dropbox',
+        ]
+    ]
 ];
